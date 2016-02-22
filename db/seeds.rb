0984ef04455ee9b84288c159227dbb98d96a5281
Jonathan Cabas <jcabas.ing@gmail.com>
2016-02-22 11:02:14 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-User.create(email: "user2@test.com", password: "Password!")
+User.find_or_create_by(email: 'user@test.com') do |user|
+  user.password = 'Password!'
+end
 2.times do
   server = Server.create(name: Faker::Lorem.word)
   task_attributes = {
@@ -7,7 +9,22 @@ User.create(email: "user2@test.com", password: "Password!")
     ended_at: Time.now + 2.hours,
     days_of_week: "mon",
     executable_path: "./var/usr",
-    server_ids: [server.id]
+    server_ids: [server.id],
+    status: "unstarted"
   }
   Task.create(task_attributes)
+end
+
+3.times do
+  server = Server.create(name: Faker::Lorem.word)
+  task_attributes = {
+    name: Faker::Lorem.word,
+    started_at: Time.now + 1.hours,
+    ended_at: Time.now + 2.hours,
+    days_of_week: "mon",
+    executable_path: "./var/usr",
+    server_ids: [server.id],
+    status: "failed"
+  }
+Task.create(task_attributes)
 end
